cda847aa7d32a9b1e0d9e2b5d7a70ecbea94acea
donnieassaf <andrew.assaf@hotmail.com>
2024-08-05 21:39:46 +1000

    add skip final snapshot

--- modules/aws_rds_cluster/main.tf
+++ modules/aws_rds_cluster/main.tf
@@ -7,6 +7,7 @@ resource "aws_rds_cluster" "main" {
   db_subnet_group_name    = aws_db_subnet_group.main.name
   vpc_security_group_ids  = [aws_security_group.main.id]
   apply_immediately       = true
+  skip_final_snapshot     = true
 }
 
 resource "aws_rds_cluster_instance" "writer" {
